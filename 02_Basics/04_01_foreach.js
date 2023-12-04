481f1b27996a3b2395935c96f1611511bd642573
Suresh Mani Devendran <121077947+SureshManiDevendran@users.noreply.github.com>
2023-12-04 12:40:36 +0000

    filter demo

---
+++ 02_Basics/04_01_foreach.js
@@ -0,0 +1,19 @@
+const coding=["js","C#","React","Angular"]
+
+// Foreach wont return values but filter will returns values 
+
+// Foreach
+
+// const result=coding.forEach( (item)=>{
+//   console.log(item)
+    // return item;
+// });
+// console.log(result);
+
+// Filters , able to return value
+
+const final=coding.filter( (item)=>{
+    return item;
+});
+
+console.log(final);
